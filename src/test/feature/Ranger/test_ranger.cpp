9d1ade96c5ddd91c670b2000b4adc5f63f6a987c
hubertzhang <hubertzhang@apache.org>
2017-03-07 16:55:11 +0800

    step 2

--- src/test/feature/Ranger/test_ranger.cpp
+++ src/test/feature/Ranger/test_ranger.cpp
@@ -24,7 +24,6 @@
 
 #include "lib/command.h"
 #include "lib/gpfdist.h"
-#include "lib/sql_util.h"
 #include "lib/string_util.h"
 
 using std::vector;
@@ -32,6 +31,13 @@ using std::string;
 using hawq::test::SQLUtility;
 using hawq::test::Command;
 
+
+TestHawqRanger::TestHawqRanger()
+{
+
+	initfile = hawq::test::stringFormat("Ranger/sql/init_file");
+	rangerHost = RANGER_HOST;
+}
 TEST_F(TestHawqRanger, BasicTest) {
     SQLUtility util;
 
@@ -157,78 +163,253 @@ TEST_F(TestHawqRanger, BasicTest) {
     }
 }
 
-TEST_F(TestHawqRanger, NegativeTest) {
+TEST_F(TestHawqRanger, DenyTest) {
     SQLUtility util;
     if (util.getGUCValue("hawq_acl_type") == "ranger")
     {
-		string rootPath(util.getTestRootPath());
-		string initfile = hawq::test::stringFormat("Ranger/sql/init_file");
-		auto cmd = hawq::test::stringFormat("ls -l %s/Ranger/sql/deny/*.sql 2>/dev/null| grep \"^-\" | wc -l", rootPath.c_str());
-		int sql_num = std::atoi(Command::getCommandOutput(cmd).c_str());
-		int writableTableCase = 28;
-		string rangerHost = RANGER_HOST;
-
-
-		// clear environment
-		for (int i = 1; i <= sql_num; i++) {
-			// delete user_num
-			std::string denyusername = hawq::test::stringFormat("userdeny%d", i);
-			util.execute(hawq::test::stringFormat("drop role %s;",denyusername.c_str()), false);
-			// delete policy
-			std::string cmd = hawq::test::stringFormat("ls -l %s/Ranger/denypolicy/%d/ 2>/dev/null| grep \"^-\" | wc -l ", rootPath.c_str(), i);
-			int policy_num = std::atoi(Command::getCommandOutput(cmd).c_str());
-			for (int j = 1; j <= policy_num; j++) {
-				cmd = hawq::test::stringFormat("python %s/Ranger/rangerpolicy.py -h %s -d denypolicy%d-%d", rootPath.c_str(), rangerHost.c_str(), i, j);
-				Command::getCommandStatus(cmd);
-			}
-		}
-
+		clearEnv(&util, "deny", 1);
 		util.execute("create table a(i int);");
+		addUser(&util, "deny", 1, true);
+		runSQLFile(&util, "deny", "succeed", 1);
+		addPolicy(&util, "deny", 1);
+		runSQLFile(&util, "deny", "fail", 1);
+    }
+}
+
+
+TEST_F(TestHawqRanger, DenyExcludeTest) {
+    SQLUtility util;
+    if (util.getGUCValue("hawq_acl_type") == "ranger")
+    {
+		clearEnv(&util, "denyexclude", 2);
+		clearEnv(&util, "deny", 2);
+		util.execute("create table a(i int);");
+		addUser(&util, "denyexclude", 2, true);
+		runSQLFile(&util, "denyexclude", "succeed", 2);
+		addPolicy(&util, "deny", 2);
+		runSQLFile(&util, "denyexclude", "fail", 2);
+		addPolicy(&util, "denyexclude", 2);
+		runSQLFile(&util, "denyexclude", "succeed2", 2);
+    }
+}
+
+TEST_F(TestHawqRanger, AllowExcludeTest) {
+    SQLUtility util;
+    if (util.getGUCValue("hawq_acl_type") == "ranger")
+    {
+		clearEnv(&util, "allowexclude", 3);
+		clearEnv(&util, "allow", 3);
+		util.execute("create table a(i int);");
+		addUser(&util, "allowexclude", 3, false);
+
+		addPolicy(&util, "allow", 3);
+		runSQLFile(&util, "allowexclude", "succeed", 3);
+
+		addPolicy(&util, "allowexclude", 3);
+		runSQLFile(&util, "allowexclude", "fail", 3);
+    }
+}
+
+TEST_F(TestHawqRanger, ResourceExcludeTest) {
+    SQLUtility util;
+    if (util.getGUCValue("hawq_acl_type") == "ranger")
+    {
+		clearEnv(&util, "resourceexclude", 4);
+		clearEnv(&util, "allow", 4);
+		util.execute("create table a(i int);");
+		util.execute("create table b(i int);");
+		addUser(&util, "resourceexclude", 4, false);
+
+		addPolicy(&util, "resourceexclude", 4);
+		// select a fail, select b succeed
+		runSQLFile(&util, "resourceexclude", "fail", 4);
+
+		//add usage-schema to public
+		addPolicy(&util, "allow", 4);
+		runSQLFile(&util, "resourceexclude", "succeed", 4);
+
+    }
+}
+
+TEST_F(TestHawqRanger, ResourceExcludeStarTest) {
+    SQLUtility util;
+    if (util.getGUCValue("hawq_acl_type") == "ranger")
+    {
+		clearEnv(&util, "resourceexclude", 5);
+		clearEnv(&util, "allow", 5);
+		util.execute("create table a(i int);");
+		addUser(&util, "resourceexclude", 5, false);
+
+		addPolicy(&util, "resourceexclude", 5);
+		// select a fail, select b succeed
+		runSQLFile(&util, "resourceexclude", "fail", 5);
+
+		//add usage-schema to public
+		addPolicy(&util, "allow", 5);
+		runSQLFile(&util, "resourceexclude", "fail2", 5);
+    }
+}
+
+TEST_F(TestHawqRanger, ResourceIncludeATest) {
+    SQLUtility util;
+    if (util.getGUCValue("hawq_acl_type") == "ranger")
+    {
+		clearEnv(&util, "allow", 6);
+		util.execute("create table a(i int);");
+		addUser(&util, "allow", 6, false);
+
+		addPolicy(&util, "allow", 6);
+		runSQLFile(&util, "allow", "fail", 6);
+    }
+}
+
+
+
+void TestHawqRanger::addUser(hawq::test::SQLUtility* util, std::string case_name, int user_index, bool full_policy, int writable_index)
+{
+	string rootPath = util->getTestRootPath();
+	string cmd = "";
+	if (user_index == -1)
+	{
+		cmd = hawq::test::stringFormat("ls -l %s/Ranger/sql/%s/*.sql 2>/dev/null| grep \"^-\" | wc -l", rootPath.c_str(), case_name.c_str());
+		int sql_num = std::atoi(Command::getCommandOutput(cmd).c_str());
 		for (int i = 1; i <= sql_num; i++) {
 			// create user_num
-			std::string denyusername = hawq::test::stringFormat("userdeny%d", i);
-			util.execute(hawq::test::stringFormat("create role %s with login createdb CREATEEXTTABLE CREATEROLE;", denyusername.c_str()),true);
-			cmd = hawq::test::stringFormat("python %s/Ranger/rangeruser.py -h %s -u %s -f True", rootPath.c_str(),
-					rangerHost.c_str(),denyusername.c_str());
-			Command::getCommandStatus(cmd);
-		}
-		sleep(60);
-
-		for (int i = 1; i <= sql_num; i++) {
-			std::string denyusername = hawq::test::stringFormat("userdeny%d", i);
-			//run sql by different users
-			string deny_sqlfile = hawq::test::stringFormat("Ranger/sql/deny/%d.sql", i);
-			string deny_ansfile_fail = hawq::test::stringFormat("Ranger/ans/deny%d_succeed.ans", i);
-
-			cmd = hawq::test::stringFormat("ls -l %s/Ranger/denypolicy/%d/ 2>/dev/null| grep \"^-\"| wc -l", rootPath.c_str(), i);
-			int policy_num = std::atoi(Command::getCommandOutput(cmd).c_str());
-
-			cmd = hawq::test::stringFormat("ls -l %s/Ranger/sql/super/%d.sql 2>/dev/null| grep \"^-\"| wc -l", rootPath.c_str(), i);
-			int supersqlexist = std::atoi(Command::getCommandOutput(cmd).c_str());
-
-			if (policy_num > 0){
-				util.execSQLFile(deny_sqlfile, deny_ansfile_fail, initfile);
+			std::string denyusername = hawq::test::stringFormat("user%s%d", case_name.c_str() ,i);
+			util->execute(hawq::test::stringFormat("create role %s with login createdb CREATEEXTTABLE CREATEROLE;", denyusername.c_str()),true);
+			if (full_policy)
+			{
+				cmd = hawq::test::stringFormat("python %s/Ranger/rangeruser.py -h %s -u %s -f True", rootPath.c_str(),
+								rangerHost.c_str(),denyusername.c_str());
+			} else
+			{
+				cmd = hawq::test::stringFormat("python %s/Ranger/rangeruser.py -h %s -u %s", rootPath.c_str(),
+								rangerHost.c_str(),denyusername.c_str());
 			}
+			Command::getCommandStatus(cmd);
+			if (full_policy)
+			{
+				sleep(60);
+			}
+		}
+	} else
+	{
+		std::string denyusername = hawq::test::stringFormat("user%s%d", case_name.c_str() ,user_index);
+		util->execute(hawq::test::stringFormat("create role %s with login createdb CREATEEXTTABLE CREATEROLE;", denyusername.c_str()),true);
+		if (full_policy)
+		{
+			cmd = hawq::test::stringFormat("python %s/Ranger/rangeruser.py -h %s -u %s -f True", rootPath.c_str(),
+							rangerHost.c_str(),denyusername.c_str());
+		} else
+		{
+			cmd = hawq::test::stringFormat("python %s/Ranger/rangeruser.py -h %s -u %s", rootPath.c_str(),
+							rangerHost.c_str(),denyusername.c_str());
+		}
+		Command::getCommandStatus(cmd);
+		if (full_policy)
+		{
+			sleep(60);
+		}
+	}
+}
 
+void TestHawqRanger::clearEnv(hawq::test::SQLUtility* util, std::string case_name, int user_index)
+{
+	string rootPath = util->getTestRootPath();
+	string cmd = "";
+	if (user_index == -1)
+	{
+		cmd = hawq::test::stringFormat("ls -l %s/Ranger/sql/%s/*.sql 2>/dev/null| grep \"^-\" | wc -l", rootPath.c_str(), case_name.c_str());
+		int sql_num = std::atoi(Command::getCommandOutput(cmd).c_str());
+		for (int i = 1; i <= sql_num; i++) {
+			// delete user_num
+			std::string denyusername = hawq::test::stringFormat("user%s%d", case_name.c_str(), i);
+			util->execute(hawq::test::stringFormat("drop role %s;",denyusername.c_str()), false);
+			// delete policy
+			std::string cmd = hawq::test::stringFormat("ls -l %s/Ranger/%spolicy/%d/ 2>/dev/null| grep \"^-\" | wc -l ", rootPath.c_str(), case_name.c_str(), i);
+			int policy_num = std::atoi(Command::getCommandOutput(cmd).c_str());
 			for (int j = 1; j <= policy_num; j++) {
-				cmd = hawq::test::stringFormat("python %s/Ranger/rangerpolicy.py -h %s -a %s/Ranger/denypolicy/%d/%d.json", rootPath.c_str(), rangerHost.c_str(), rootPath.c_str(), i, j);
+				cmd = hawq::test::stringFormat("python %s/Ranger/rangerpolicy.py -h %s -d %spolicy%d-%d", rootPath.c_str(), rangerHost.c_str(), case_name.c_str(), i, j);
 				Command::getCommandStatus(cmd);
 			}
 		}
-		sleep(60);
-		for (int i = 1; i <= sql_num; i++) {
-			std::string denyusername = hawq::test::stringFormat("userdeny%d", i);
-			//run sql by different users
-			string deny_sqlfile = hawq::test::stringFormat("Ranger/sql/deny/%d.sql", i);
-			string deny_ansfile_fail = hawq::test::stringFormat("Ranger/ans/deny%d_fail.ans", i);
+	} else
+	{
+		// delete user_num
+		std::string denyusername = hawq::test::stringFormat("user%s%d", case_name.c_str(), user_index);
+		util->execute(hawq::test::stringFormat("drop role %s;",denyusername.c_str()), false);
+		// delete policy
+		std::string cmd = hawq::test::stringFormat("ls -l %s/Ranger/%spolicy/%d/ 2>/dev/null| grep \"^-\" | wc -l ", rootPath.c_str(), case_name.c_str(), user_index);
+		int policy_num = std::atoi(Command::getCommandOutput(cmd).c_str());
+		for (int j = 1; j <= policy_num; j++) {
+			cmd = hawq::test::stringFormat("python %s/Ranger/rangerpolicy.py -h %s -d %spolicy%d-%d", rootPath.c_str(), rangerHost.c_str(), case_name.c_str(), user_index, j);
+			Command::getCommandStatus(cmd);
+		}
+	}
+}
 
-			cmd = hawq::test::stringFormat("ls -l %s/Ranger/denypolicy/%d/ 2>/dev/null| grep \"^-\"| wc -l", rootPath.c_str(), i);
+
+void TestHawqRanger::runSQLFile(hawq::test::SQLUtility* util, std::string case_name, std::string ans_suffix, int sql_index)
+{
+	string rootPath = util->getTestRootPath();
+	auto cmd = hawq::test::stringFormat("ls -l %s/Ranger/sql/%s/*.sql 2>/dev/null| grep \"^-\" | wc -l", rootPath.c_str(), case_name.c_str());
+
+	// run all the sql files in folder.
+	if(sql_index == -1)
+	{
+		int sql_num = std::atoi(Command::getCommandOutput(cmd).c_str());
+		for (int i = 1; i <= sql_num; i++) {
+			string deny_sqlfile = hawq::test::stringFormat("Ranger/sql/%s/%d.sql", i, case_name.c_str());
+			string deny_ansfile_succeed = hawq::test::stringFormat("Ranger/ans/%s%d_%s.ans", case_name.c_str(), i, ans_suffix.c_str());
+
+			auto cmd = hawq::test::stringFormat("ls -l %s/Ranger/%spolicy/%d/ 2>/dev/null| grep \"^-\"| wc -l", rootPath.c_str(), case_name.c_str(), i);
 			int policy_num = std::atoi(Command::getCommandOutput(cmd).c_str());
 
 			if (policy_num > 0){
-				util.execSQLFile(deny_sqlfile, deny_ansfile_fail, initfile);
+				util->execSQLFile(deny_sqlfile, deny_ansfile_succeed, initfile);
 			}
-
 		}
-    }
+	} else
+	{
+		string deny_sqlfile = hawq::test::stringFormat("Ranger/sql/%s/%d.sql", case_name.c_str(), sql_index);
+		string deny_ansfile_succeed = hawq::test::stringFormat("Ranger/ans/%s%d_%s.ans", case_name.c_str(), sql_index, ans_suffix.c_str());
+
+		auto cmd = hawq::test::stringFormat("ls -l %s/Ranger/%spolicy/%d/ 2>/dev/null| grep \"^-\"| wc -l", rootPath.c_str(), case_name.c_str(), sql_index);
+		int policy_num = std::atoi(Command::getCommandOutput(cmd).c_str());
+
+		if (policy_num > 0){
+			util->execSQLFile(deny_sqlfile, deny_ansfile_succeed, initfile);
+		}
+	}
+
+}
+
+void TestHawqRanger::addPolicy(hawq::test::SQLUtility* util, std::string case_name, int policy_index)
+{
+	string rootPath = util->getTestRootPath();
+	auto cmd = hawq::test::stringFormat("ls -l %s/Ranger/sql/%s/*.sql 2>/dev/null| grep \"^-\" | wc -l", rootPath.c_str(), case_name.c_str());
+	if (policy_index == -1)
+	{
+		int sql_num = std::atoi(Command::getCommandOutput(cmd).c_str());
+		for (int i = 1; i <= sql_num; i++) {
+			auto cmd = hawq::test::stringFormat("ls -l %s/Ranger/%spolicy/%d/ 2>/dev/null| grep \"^-\"| wc -l", rootPath.c_str(), case_name.c_str(), i);
+			int policy_num = std::atoi(Command::getCommandOutput(cmd).c_str());
+
+
+			for (int j = 1; j <= policy_num; j++) {
+				cmd = hawq::test::stringFormat("python %s/Ranger/rangerpolicy.py -h %s -a %s/Ranger/%spolicy/%d/%d.json", rootPath.c_str(), rangerHost.c_str(), rootPath.c_str(), case_name.c_str(), i, j);
+				Command::getCommandStatus(cmd);
+			}
+		}
+	} else
+	{
+		auto cmd = hawq::test::stringFormat("ls -l %s/Ranger/%spolicy/%d/ 2>/dev/null| grep \"^-\"| wc -l", rootPath.c_str(), case_name.c_str(), policy_index);
+		int policy_num = std::atoi(Command::getCommandOutput(cmd).c_str());
+
+		for (int j = 1; j <= policy_num; j++) {
+			cmd = hawq::test::stringFormat("python %s/Ranger/rangerpolicy.py -h %s -a %s/Ranger/%spolicy/%d/%d.json", rootPath.c_str(), rangerHost.c_str(), rootPath.c_str(), case_name.c_str(), policy_index, j);
+			Command::getCommandStatus(cmd);
+		}
+	}
+	sleep(60);
 }
